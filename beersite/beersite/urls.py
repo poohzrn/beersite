18b4b803561c3b58596b324fde2734afaa805356
Lasse Nielsen <me@lasse.rocks>
2016-05-05 13:02:55 +0200

    Redirect / to index view

--- beersite/beersite/urls.py
+++ beersite/beersite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
+from beerstats.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^beer/', include('beerstats.urls')),
+    url(r'^$', index),
 ]
